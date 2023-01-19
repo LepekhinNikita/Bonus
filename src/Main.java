5cc5ff9d1a0ade9007fee8dbc4a580582a9c3484
Lepehin Nikita <Lepekhinnikita@gmail.com>
2023-01-19 22:00:29 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 1019; // стоимость билета
+
+        System.out.println("Бонусные мили = " + (amount / 20));
+
+    }
+}
